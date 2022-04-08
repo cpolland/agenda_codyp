aa68955d7ddd6a28165c17aba18e58c546e79f8b
Cody Polland <cody.polland@gmail.com>
2022-04-07 22:26:12 -0700

    Function experimentation

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 var Time = moment();
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+
+function color(){
+    $(".time-block").each(function(){
+    var setTime = parseInt($(this).attr("id").replace("hour-",''));
+    var currentTime = parseInt(moment().format('H'));
+
+    console.log('setTime',setTime)
+    console.log('currentTime',currentTime)
+    $(this).removeClass("past present future");
+    if (setTime < currentTime){
+        console.log(setTime, currentTime);
+    }
+
+}
